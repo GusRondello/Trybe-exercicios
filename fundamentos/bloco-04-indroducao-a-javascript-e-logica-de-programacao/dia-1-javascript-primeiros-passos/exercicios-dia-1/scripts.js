999644828e4a3bae2116d92ebda7624fb6226ba4
Gustavo Rondello <gustavorondello99@outlook.com>
2021-11-26 19:01:10 -0300

    Feitos exercicios de 1 á 7

---
+++ fundamentos/bloco-04-indroducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-dia-1/scripts.js
@@ -0,0 +1,116 @@
+//Definindo constantes para exercicios:
+
+/* const a = 8;
+const b = 4;
+const c = 6; */
+
+//Exercicio 1:
+
+/* const adicao = a + b
+const subtracao = a - b;
+const multiplicacao = a * b;
+const divisao = a / b;
+const modulo = a % b;
+
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo); */
+
+//Exercicio 2:
+
+/* if(a > b){
+    console.log(a);
+}else{
+    console.log(b);
+} */
+
+//Exercicio 3:
+
+/* if(a > b && a > c){
+    console.log(a);
+}
+else if(b > a && b > c){
+    console.log(b);
+}else {
+    console.log(c);
+} */
+
+//Exercicio 4:
+
+/* if(a > 0){
+    console.log("positive");
+}
+else if (a < 0){
+    console.log("negative");
+}else{
+    console.log("zero");
+} */
+
+//Exercicio 5:
+
+/* if (a < 0){
+    console.log("ERROR");
+}else if (b < 0){
+    console.log("ERROR");
+}else if (c < 0){
+    console.log("ERROR");
+}else{
+    if(a + b + c === 180){
+        console.log(true);
+    }else if (a + b + c <= 180){
+        console.log(false);
+    }
+} */
+
+//Exercicio 6
+
+/* let peca = "";
+
+switch (peca.toLowerCase()) {
+    case "peao":
+        console.log("Move-se uma casa por vez para frente.");
+    break;
+    case "bispo":
+        console.log("Move-se quantas casas quiser na diagonal.");
+    break;
+    case "torre":
+        console.log("Move-se quantas casas quiser na vertical.");
+    break;
+    case "cavalo":
+        console.log("Move-se em L.");
+    break;
+    case "rainha":
+        console.log("Move-se quantas casas quiser em qualquer direcao.");
+    break;
+    case "rei":
+        console.log("Move-se uma casa por vez em qualquer direcao.");
+    break;
+    default:
+        console.log("Peça invalida.");
+} */
+
+//Exercicio 7
+
+/* let nota = 49
+
+if(nota >= 90 && nota <= 100 && nota > 0){
+    console.log("Nota A!")
+}
+else if(nota >= 80 && nota <= 100 && nota > 0){
+    console.log("Nota B!")
+}
+else if(nota >= 70 && nota <= 100 && nota > 0){
+    console.log("Nota C!")
+}
+else if(nota >= 60 && nota <= 100 && nota > 0){
+    console.log("Nota D!")
+}
+else if(nota >= 50 && nota <= 100 && nota > 0){
+    console.log("Nota E!")
+}else if(nota < 50 && nota >= 0 && nota <= 100){
+    console.log("Nota F!")
+}else {
+    console.log("ERROR: Nota inválida.")
+}  */
