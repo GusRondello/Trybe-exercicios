d44965c27ae6aab3fb63862085ab806238f3b74c
Gustavo Rondello <gustavorondello99@outlook.com>
2021-12-06 15:22:21 -0300

    Feito aula course parte 1

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/aula-course-5.2/parte1/script.js
@@ -0,0 +1,23 @@
+// 1
+document.getElementById("elementoOndeVoceEsta");
+
+// 2
+document.getElementById("elementoOndeVoceEsta").parentNode.style.color = "red";
+
+// 3
+document.getElementById("primeiroFilhoDoFilho").textContent = "Sim me lembro.";
+
+// 4
+document.getElementById("pai").firstElementChild;
+
+// 5
+document.getElementById("elementoOndeVoceEsta").firstElementChild;
+
+// 6
+document.getElementById("elementoOndeVoceEsta").nextSibling;
+
+// 7
+document.getElementById("elementoOndeVoceEsta").nextElementSibling;
+
+// 8
+document.getElementById("pai").lastElementChild.previousElementSibling;
