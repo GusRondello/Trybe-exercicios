2ab7b699983e596cbe3ebe509b884f52e1f7b575
Gustavo Rondello <gustavorondello99@outlook.com>
2021-12-06 18:02:59 -0300

    Feito exercicios parte 2 aula course

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/aula-course-5.2/parte1/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/aula-course-5.2/parte1/script.js
@@ -1,4 +1,17 @@
-// 1
+/*parentNode : retorna o nó pai.
+parentElement : retorna o elemento pai.
+childNodes : retorna um NodeList com todos os nós filhos.
+children : retorna um HTMLCollection com todos os elementos filhos.
+firstChild : retorna o primeiro nó filho.
+firstElementChild : retorna o primeiro elemento filho.
+lastChild : retorna o último nó filho.
+lastElementChild : retorna o último elemento filho.
+nextSibling : retorna o próximo nó.
+nextElementSibling : retorna o próximo elemento.
+previousSibling : retorna o nó anterior.
+previousElementSibling : retorna o elemento anterior.*/
+
+/* // 1
 document.getElementById("elementoOndeVoceEsta");
 
 // 2
@@ -21,3 +34,35 @@ document.getElementById("elementoOndeVoceEsta").nextElementSibling;
 
 // 8
 document.getElementById("pai").lastElementChild.previousElementSibling;
+
+// parte 2  CONSOLE LOG COMANDOS:
+// 1 */
+
+/*
+let novoFilhoText = "Estou aqui!";
+let novoFilho = document.createElement('section');
+novoFilho.innerText = novoFilhoText;
+document.querySelector('#pai').appendChild(novoFinho)
+*/
+
+// 2 
+/*
+let filhoNovo2Text = "Mais um filho!";
+let novoFilho2 = document.createElement("section");
+novoFilho2.innerText = filhoNovo2Text;
+document.querySelector("#elementoOndeVoceEsta").appendChild(novoFilho2);
+*/
+
+// 3
+/* 
+let filhoNovo3Text = "Filho do filho!";
+let novoFilho3 = document.createElement("section");
+novoFilho3.innerText = filhoNovo3Text;
+document.querySelector("#primeiroFilhoDoFilho").appendChild(novoFilho3);
+*/
+
+// 4
+/*
+novoFilho3.id = filhoDoFilhoDoFilhoDoFilho;
+document.querySelector("#filhoDoFilhoDoFilhoDoFilho").parentElement.parentElement.nextElementSibling;
+*/
