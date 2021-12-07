f32f945ec6fb835484eff50a2ad3dbd29e446a9f
Gustavo Rondello <gustavorondello99@outlook.com>
2021-12-07 20:04:16 -0300

    Fazendo exercicios do course

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/exercicios-5.3/main.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/exercicios-5.3/main.js
@@ -4,16 +4,33 @@ const thirdLi = document.getElementById('third-li');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('my-spotrybefy');
 
-
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+// R: A .tech esta determinada a iniciar a -20px em y das outras.
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+function addClass (event) { 
+  const techClass = document.querySelector(".tech");
+  techClass.classList.remove("tech");
+  event.target.className = "tech";
+}
+
+firstLi.addEventListener("click", addClass);
+secondLi.addEventListener("click", addClass);
+thirdLi.addEventListener("click", addClass);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+function classByText (event) {
+  const techClass = document.querySelector(".tech");
+  techClass.innerText = "teste"; //isso faz com que toda vez que eu escreva algo no input ele reescreva como teste no selecionado.
+
+  //obs preciso coletar o texto que estou escrevendo no input e "colar" no techClass
+}
+
+input.addEventListener("input", classByText);
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
